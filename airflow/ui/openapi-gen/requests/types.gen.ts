a04ea5b2328f7a40e5d20fab1e82260d1143292b
LIU ZHE YOU <zhu424.dev@gmail.com>
2025-02-25 22:42:12 +0800

    AIP-84 | Add Auth for Dag

--- airflow/ui/openapi-gen/requests/types.gen.ts
+++ airflow/ui/openapi-gen/requests/types.gen.ts
@@ -1987,6 +1987,7 @@ export type ListDagWarningsResponse = DAGWarningCollectionResponse;
 
 export type GetDagsData = {
   dagDisplayNamePattern?: string | null;
+  dagId?: string | null;
   dagIdPattern?: string | null;
   dagRunEndDateGte?: string | null;
   dagRunEndDateLte?: string | null;
@@ -2007,6 +2008,7 @@ export type GetDagsData = {
 export type GetDagsResponse = DAGCollectionResponse;
 
 export type PatchDagsData = {
+  dagId?: string | null;
   dagIdPattern?: string | null;
   lastDagRunState?: DagRunState | null;
   limit?: number;
@@ -2023,13 +2025,13 @@ export type PatchDagsData = {
 export type PatchDagsResponse = DAGCollectionResponse;
 
 export type GetDagData = {
-  dagId: string;
+  dagId: string | null;
 };
 
 export type GetDagResponse = DAGResponse;
 
 export type PatchDagData = {
-  dagId: string;
+  dagId: string | null;
   requestBody: DAGPatchBody;
   updateMask?: Array<string> | null;
 };
@@ -2037,13 +2039,13 @@ export type PatchDagData = {
 export type PatchDagResponse = DAGResponse;
 
 export type DeleteDagData = {
-  dagId: string;
+  dagId: string | null;
 };
 
 export type DeleteDagResponse = unknown;
 
 export type GetDagDetailsData = {
-  dagId: string;
+  dagId: string | null;
 };
 
 export type GetDagDetailsResponse = DAGDetailsResponse;
