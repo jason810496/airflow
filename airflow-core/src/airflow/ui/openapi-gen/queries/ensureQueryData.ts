26802bb2372f77ce29d901d672d876ca65f3d06e
LIU ZHE YOU <zhu424.dev@gmail.com>
2025-04-14 22:59:15 +0800

    Add ndjson header to get_log openapi schema

--- airflow-core/src/airflow/ui/openapi-gen/queries/ensureQueryData.ts
+++ airflow-core/src/airflow/ui/openapi-gen/queries/ensureQueryData.ts
@@ -1664,7 +1664,7 @@ export const ensureUseTaskInstanceServiceGetLogData = (
     token,
     tryNumber,
   }: {
-    accept?: "application/json" | "text/plain" | "*/*";
+    accept?: "application/json" | "*/*" | "application/x-ndjson";
     dagId: string;
     dagRunId: string;
     fullContent?: boolean;
