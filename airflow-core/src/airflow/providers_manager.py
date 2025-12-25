7697bed339a5c106dbb31d7fda8f72408c638805
LIU ZHE YOU <zhu424.dev@gmail.com>
2025-12-25 16:27:20 +0800

    Final refactor for ProviderManager

--- airflow-core/src/airflow/providers_manager.py
+++ airflow-core/src/airflow/providers_manager.py
@@ -406,6 +406,7 @@ class ProvidersManager(LoggingMixin, metaclass=Singleton):
         self._connection_form_widgets: dict[str, ConnectionFormWidgetInfo] = {}
         # Customizations for javascript fields are kept here
         self._field_behaviours: dict[str, dict] = {}
+        self._cli_command_function_name_set: set[str] = set()
         self._extra_link_class_name_set: set[str] = set()
         self._logging_class_name_set: set[str] = set()
         self._auth_manager_class_name_set: set[str] = set()
@@ -421,8 +422,6 @@ class ProvidersManager(LoggingMixin, metaclass=Singleton):
         )
         # Set of plugins contained in providers
         self._plugins_set: set[PluginInfo] = set()
-        # CLI command functions from providers
-        self._cli_command_function_names: list[str] = []
         self._init_airflow_core_hooks()
 
     def _init_airflow_core_hooks(self):
@@ -1077,6 +1076,14 @@ class ProvidersManager(LoggingMixin, metaclass=Singleton):
                     if _correctness_check(provider_package, auth_manager_class_name, provider):
                         self._auth_manager_class_name_set.add(auth_manager_class_name)
 
+    def _discover_cli_commands(self) -> None:
+        """Retrieve all CLI command functions defined in the providers."""
+        for provider_package, provider in self._provider_dict.items():
+            if provider.data.get("cli"):
+                for cli_command_function_name in provider.data["cli"]:
+                    if _correctness_check(provider_package, cli_command_function_name, provider):
+                        self._cli_command_function_name_set.add(cli_command_function_name)
+
     def _discover_notifications(self) -> None:
         """Retrieve all notifications defined in the providers."""
         for provider_package, provider in self._provider_dict.items():
@@ -1146,16 +1153,6 @@ class ProvidersManager(LoggingMixin, metaclass=Singleton):
                     )
                 )
 
-    def _discover_cli_commands(self) -> None:
-        """Retrieve all CLI command functions defined in the providers."""
-        for provider_package, provider in self._provider_dict.items():
-            cli_items = provider.data.get("cli", [])
-            if cli_items:
-                for cli_item in cli_items:
-                    # cli_item is now a string directly
-                    if isinstance(cli_item, str):
-                        self._cli_command_function_names.append(cli_item)
-
     @provider_info_cache("triggers")
     def initialize_providers_triggers(self):
         """Initialize providers triggers."""
@@ -1176,6 +1173,12 @@ class ProvidersManager(LoggingMixin, metaclass=Singleton):
         """Returns information about available providers notifications class."""
         self.initialize_providers_auth_managers()
         return sorted(self._auth_manager_class_name_set)
+
+    @property
+    def cli_command_function_names(self) -> list[str]:
+        """Returns list of CLI command function names from providers."""
+        self.initialize_providers_cli_commands()
+        return sorted(self._cli_command_function_name_set)
 
     @property
     def notification(self) -> list[NotificationInfo]:
@@ -1301,12 +1304,6 @@ class ProvidersManager(LoggingMixin, metaclass=Singleton):
     def already_initialized_provider_configs(self) -> list[tuple[str, dict[str, Any]]]:
         return sorted(self._provider_configs.items(), key=lambda x: x[0])
 
-    @property
-    def cli_command_function_names(self) -> list[str]:
-        """Returns list of CLI command function names from providers."""
-        self.initialize_providers_cli_commands()
-        return self._cli_command_function_names
-
     def _cleanup(self):
         self._initialized_cache.clear()
         self._provider_dict.clear()
@@ -1327,7 +1324,7 @@ class ProvidersManager(LoggingMixin, metaclass=Singleton):
         self._trigger_info_set.clear()
         self._notification_info_set.clear()
         self._plugins_set.clear()
-        self._cli_command_function_names.clear()
+        self._cli_command_function_name_set.clear()
 
         self._initialized = False
         self._initialization_stack_trace = None
